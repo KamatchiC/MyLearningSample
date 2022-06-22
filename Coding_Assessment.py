1e6c0a97222aeb3ed87b48584b5df094f47772ab
kamatchi <ambiv.1983@gmail.com>
2022-06-21 22:52:22 -0400

    Tuplesinpython
    Signed-off-by: kamatchi <ambiv.1983@gmail.com>

--- Coding_Assessment.py
+++ Coding_Assessment.py
@@ -1,12 +1,12 @@
 import os
+import csv
 import numpy as np
 import pandas as pd
 import psycopg2
-df = pd.read_csv("C:\\Users\\Employee_data.csv")
-
+df = pd.read_csv("C:\\kamatchi\\Employee_data.csv")
 print(df)
 
-# tablename cleanup
+# Tablename cleanup
 file = "Employee_Data"
 clean_tbl_name = file.lower()
 print(clean_tbl_name)
@@ -21,22 +21,36 @@ replacements = {
 
 print(replacements)
 
-col_str =  ", ".join("{} {}".format(n,d) for (n,d) in zip(df.columns,df.dtypes.replace(replacements)))
+col_str = ", ".join("{} {}".format(n,d) for (n,d) in zip(df.columns,df.dtypes.replace(replacements)))
 print(col_str)
 
+conn = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='localhost')
+cur = conn.cursor()
+print("opened database successfully")
 
 #drop tables with same name
+cur.execute("drop table if exists Emp_data")
 
-cursor.execute("drop table if exists employeedata")
-create table employeedata(empid int, ename varchar, dept varchar)
+#Create table
+cur.execute("CREATE TABLE Emp_data(empid int, ename varchar, dept varchar)")
 
 #inserting values to table
+df.to_csv('C:\\kamatchi\\Emp_data.csv', header=df.columns, index=False, encoding='utf-8')
+my_file = open('C:\\kamatchi\\Emp_data.csv')
+print('file opened in memory')
 
-df.to_csv('employeedata.csv',header=df.columns, index=False)
-myfile=open('employeedata.csv')
-print('file opened')
 
 #Copying data to database
-SQLSTATEMENT = COPY employeedata from employeedate with csv HEADER DELIMITER AS ','
-cursor.copy.expert(sql=SQL_STATEMENT, file=myfile)
+SQL_STATEMENT = """
+COPY Emp_data from STDIN WITH 
+CSV 
+HEADER 
+DELIMITER AS ','
+"""
 
+cur.copy_expert(sql=SQL_STATEMENT, file=my_file)
+print('file copied')
+
+cur.execute("Grant select on table Emp_Data to public")
+conn.commit()
+conn.close()

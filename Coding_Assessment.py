939352a33c36be823477b39ce731d8a633649197
kamatchi <ambiv.1983@gmail.com>
2022-07-14 17:17:53 -0400

    Tuplesinpython
    Signed-off-by: kamatchi <ambiv.1983@gmail.com>

--- Coding_Assessment.py
+++ Coding_Assessment.py
@@ -1,8 +1,7 @@
-import os
-import csv
-import numpy as np
 import pandas as pd
+import os
 import psycopg2
+
 df = pd.read_csv("C:\\kamatchi\\Employee_data.csv")
 print(df)
 
@@ -10,31 +9,30 @@ print(df)
 file = "Employee_Data"
 clean_tbl_name = file.lower()
 print(clean_tbl_name)
-
-df.columns = [x.lower().replace("_","") for x in df.columns]
+df.columns = [x.lower().replace("_", "") for x in df.columns]
 print(df.columns)
 print(df.dtypes)
 replacements = {
-    'object':'varchar',
-    'int64':'int'
+    'object': 'varchar',
+    'int64': 'int'
 }
 
 print(replacements)
 
-col_str = ", ".join("{} {}".format(n,d) for (n,d) in zip(df.columns,df.dtypes.replace(replacements)))
+col_str = ", ".join("{} {}".format(n, d) for (n, d) in zip(df.columns, df.dtypes.replace(replacements)))
 print(col_str)
 
 conn = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='localhost')
 cur = conn.cursor()
 print("opened database successfully")
 
-#drop tables with same name
+# drop tables with same name
 cur.execute("drop table if exists Emp_data")
 
-#Create table
+# Create table
 cur.execute("CREATE TABLE Emp_data(empid int, ename varchar, dept varchar)")
 
-#inserting values to table
+# inserting values to table
 df.to_csv('C:\\kamatchi\\Emp_data.csv', header=df.columns, index=False, encoding='utf-8')
 my_file = open('C:\\kamatchi\\Emp_data.csv')
 print('file opened in memory')
